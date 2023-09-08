f86ad8a2ffdad2a8f108a1a96e55b96fce0bff32
mrenigmatic19 <96334291+mrenigmatic19@users.noreply.github.com>
2023-09-08 14:36:29 +0530

    --

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,42 @@ app.get("/",async(req,res)=>{
     res.render("index")
 })
 
+app.get("/about",async(req,res)=>{
+    res.render("about")
+})
+
+app.get("/blog",async(req,res)=>{
+    res.render("blog")
+})
+
+app.get("/contact",async(req,res)=>{
+    res.render("contact")
+})
+
+app.get("/detail",async(req,res)=>{
+    res.render("detail")
+})
+
+app.get("/price",async(req,res)=>{
+    res.render("price")
+})
+
+app.get("/product",async(req,res)=>{
+    res.render("product")
+})
+
+app.get("/service",async(req,res)=>{
+    res.render("service")
+})
+
+app.get("/team",async(req,res)=>{
+    res.render("team")
+})
+
+app.get("/testimonial",async(req,res)=>{
+    res.render("testimonial")
+})
+
 app.listen(port,hostname,()=>{
     console.log("Server is Running..")
 })
